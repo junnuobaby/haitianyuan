7cc7875b61d8a2d46dfcd5628f2c7cfe959166cb
tangchaohong <444092487@qq.com>
2015-08-04 16:21:56 +0800

    update the register_user_info page

--- application/views/pages/register_user_info.php
+++ application/views/pages/register_user_info.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <?php $this->load->view('./templates/navbar'); ?>
     <?php $this->load->view('./templates/jumptron'); ?>
     <div class="container">
-        <div class="col-md-10">
+        <div class="col-md-10 col-md-offset-1">
             <div class="page-header">
                 <h3>完善个人资料
                     <small>普通用户选择会员，理财师用户选择理财师进行个人资料完善</small>
@@ -33,19 +33,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <div class="tab-content">
                         <!-- 会员个人资料完善 -->
                         <div role="tabpanel" class="tab-pane active" id="user" aria-labelledby="user-tab">
-                            <div class="col-md-5 col-md-offset-4">
+                            <div class="col-md-5 col-md-offset-2">
                                 <form>
                                     <br/>
 
                                     <div class="form-group has-feedback">
-                                        <label for="username">用户名</span></label>
-                                        <input type="text" class="form-control" id="username" placeholder="用户名">
+                                        <label for="username">用户名 <span class="key_info">*</span></span></label>
+                                        <input type="text" class="form-control" id="username" name="username"
+                                               placeholder="用户名">
                                         <span class="glyphicon glyphicon-user form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
-                                        <label for="email">邮箱 </label>
-                                        <input type="email" class="form-control" id="email" placeholder="e-mail">
+                                        <label for="email">邮箱 <span class="key_info">*</span> </label>
+                                        <input type="email" class="form-control" id="user_email" name="user_email"
+                                               placeholder="e-mail">
                                         <span class="glyphicon glyphicon-envelope form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
@@ -53,53 +55,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <button type="submit" class="btn btn-lg btn-danger btn-block">确定</button>
                                 </form>
                             </div>
+                            <div class="col-md-4 col-md-offset-1">
+                                <br/><br/><br/>
+
+                                <p><strong>1.</strong> 带 <span class="key_info">*</span> 号为必填项</p>
+
+                                <p><strong>2. </strong>为正常使用，请使用国内邮箱。</p>
+                            </div>
+
                         </div>
                         <!-- 理财师个人资料完善 -->
                         <div role="tabpanel" class="tab-pane" id="adviser" aria-labelledby="adviser-tab">
-                            <div class="col-md-5 col-md-offset-4 ">
+                            <div class="col-md-5 col-md-offset-2 ">
                                 <form>
                                     <br/>
 
                                     <div class="form-group has-feedback">
-                                        <label for="username">用户名</span></label>
-                                        <input type="text" class="form-control" id="username" placeholder="用户名">
+                                        <label for="username">用户名 <span class="key_info">*</span></label>
+                                        <input type="text" class="form-control" id="master" name="master"
+                                               placeholder="用户名">
                                         <span class="glyphicon glyphicon-user form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
-                                        <label for="email">邮箱 </label>
-                                        <input type="email" class="form-control" id="email" placeholder="e-mail">
+                                        <label for="email">邮箱 <span class="key_info">*</span> </label>
+                                        <input type="email" class="form-control" id="master_email" name="master_email"
+                                               placeholder="e-mail">
                                         <span class="glyphicon glyphicon-envelope form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
                                         <label for="company">机构 </label>
-                                        <input type="text" class="form-control" id="company" placeholder="机构名称">
+                                        <input type="text" class="form-control" id="master_company"
+                                               name="master_company" placeholder="机构名称">
                                         <span class="glyphicon glyphicon-th form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
                                         <label for="certification_code">资格证号码 </label>
-                                        <input type="text" class="form-control" id="certification_code"
+                                        <input type="text" class="form-control" id="master_code" name="master_code"
                                                placeholder="资质号码">
                                         <span class="glyphicon glyphicon-search form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
-                                        <label for="adviser_name">真实姓名 </label>
-                                        <input type="text" class="form-control" id="adviser_name" placeholder="您的真实姓名">
+                                        <label for="adviser_name">真实姓名 <span class="key_info">*</span> </label>
+                                        <input type="text" class="form-control" id="master_name" name="master_name"
+                                               placeholder="您的真实姓名">
                                         <span class="glyphicon glyphicon-sunglasses form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group has-feedback">
-                                        <label for="id_code">身份证号码 </label>
-                                        <input type="text" class="form-control" id="id_code" placeholder="身份证号码">
+                                        <label for="id_code">身份证号码 <span class="key_info">*</span></label>
+                                        <input type="text" class="form-control" id="master_idc" name="master_idc"
+                                               placeholder="身份证号码">
                                         <span class="glyphicon glyphicon-eye-open form-control-feedback"
                                               aria-hidden="true"></span>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputFile">身份证照片</label>
-                                        <input type="file" id="exampleInputFile">
+                                        <label for="exampleInputFile">身份证照片 <span class="key_info">*</span></label>
+                                        <input type="file" id="master_idc_pic" name="master_idc_pic">
 
                                         <p class="help-block">请上传你的身份证正面照片</p>
                                     </div>
@@ -108,6 +123,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <button type="submit" class="btn btn-lg btn-danger btn-block ">确定</button>
                                 </form>
                             </div>
+
+                            <div class="col-md-4 col-md-offset-1">
+                                <br/><br/><br/>
+
+                                <p><strong>1.</strong> 带 <span class="key_info">*</span> 号为必填项</p>
+
+                                <p><strong>2. </strong>为正常使用，请使用国内邮箱。</p>
+
+                                <p><strong>3.</strong> 机构和资格证号码为选填。</p>
+
+                                <p><strong>4.</strong> 请上传身份证正面照。</p>
+
+                                <p><strong>5. </strong>我们确保不会泄露您的个人信息。</p>
+
+                            </div>
+
                         </div>
                     </div>
                 </div>
