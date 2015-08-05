f652310fbec49d99abdec0acfc36bcf0dd76d5c8
tangchaohong <444092487@qq.com>
2015-08-05 19:14:43 +0800

    finish the profile menu

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -11,7 +11,7 @@ class Login extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('pages/weibo_login');
+        $this->load->view('pages/login');
     }
 
 
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,5 +1,5 @@
 <!--导航栏-->
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top" xmlns="http://www.w3.org/1999/html">
     <div class="container">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -23,33 +23,16 @@
                 </ul>
 
                 <!--                根据是否登陆导航条显示不同的内容，当前默认为未登录-->
-                <?php $login_state = true ?>
+                <?php $login_state = false ?>
                 <?php if ($login_state == false): ?>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="<?php echo base_url('/index.php/register') ?>">注册<span>&nbsp</span></a></li>
-                        <li><a href="<?php echo base_url('/index.php/register/user_info') ?>">
-                                <img src="<?php echo base_url('/assets/images/qq.png'); ?>"
-                                     alt="..." class="img-rounded narbar_li"></a></li>
-                        <li><a href="<?php echo base_url('/index.php/login/weibo_login') ?>">
-                                <img src="<?php echo base_url('/assets/images/weibo.png'); ?>"
-                                     alt="..." class="img-rounded narbar_li"></a></li>
-                    </ul>
+                    <div class="navbar-form navbar-right ">
+                        <a href="<?php echo base_url('/index.php/login') ?>"><input class="btn btn-danger" type="button"
+                                                                                    id="login" value="登陆"/></a>
+                        <a href="<?php echo base_url('/index.php/register') ?>"><input class="btn btn-danger"
+                                                                                       type="button" id="register"
+                                                                                       value="注册"/></a>
+                    </div>
 
-                    <form class="navbar-form navbar-right">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="user_name_input" name="user_name_input"
-                                   placeholder="邮箱 / 手机号码">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="password_input" name="password_input"
-                                   placeholder="密码">
-                        </div>
-                        <button class="btn btn-danger" type="submit" id="login">登陆</button>
-
-                        <a href="#" data-toggle="tooltip" data-placement="bottom" title="忘记密码"><span>&nbsp</span><span
-                                class="glyphicon glyphicon-question-sign"></span></a>
-                    </form>
                 <?php else: ?>
                     <!--                    登陆成功后导航条上显示的内容-->
                     <ul class="nav navbar-nav navbar-right">
@@ -59,7 +42,7 @@
                                 君诺宝宝 <span class="caret"></span></a>
 
 
-                            <ul class="dropdown-menu" >
+                            <ul class="dropdown-menu dropdown-menu-profile">
                                 <li id="info_dropdown">
                                     <div class="row">
                                         <div class="col-md-5">
@@ -67,11 +50,30 @@
                                                  src="<?php echo base_url('/assets/images/touxiang/1.jpg'); ?>"
                                                  alt="...">
                                         </div>
-
                                         <div class="col-md-7">
                                             <a href="#" class="btn btn-default">个人资料</a>
                                             <a href="#" class="btn btn-default">退出</a>
+                                            <div class="panel panel-default sign">
+                                                <div class="panel-body">
+                                                    我是一个萌哒哒的小妹子
+                                                </div>
+                                            </div>
                                         </div>
+                                    </div>
+                                    <div class="row" style="padding-top: 1em;">
+                                        <div class="col-md-1"></div>
+                                        <div class="col-md-3">
+                                            <a href="#"> 关注 <?php echo '14' ?></a>
+                                        </div>
+                                        <div style='width:1px;border:1px solid black;float:left;height:15px;'></div>
+                                        <div class="col-md-3">
+                                            <a href="#"> 粉丝 <?php echo '20' ?></a>
+                                        </div>
+                                        <div style='width:1px;border:1px solid black;float:left;height:15px;'></div>
+                                        <div class="col-md-3">
+                                            <a href="#"> 购买 <?php echo '0' ?></a>
+                                        </div>
+
 
                                     </div>
                                 </li>
